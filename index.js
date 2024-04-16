8de29003cf68851ac1092124ec258e81a559edce
AmyM6 <101350236+AmyM6@users.noreply.github.com>
2024-04-15 21:08:23 -0600

    Created a simple sentence using typescript

---
+++ index.js
@@ -0,0 +1,4 @@
+function greeting(name) {
+    return "Hello, my name is " + name + " and I love coding.";
+}
+console.log(greeting("Amy"));
---
+++ index.ts
@@ -0,0 +1,5 @@
+function greeting(name: string) {
+    return "Hello, my name is " + name + " and I love coding.";
+}
+
+console.log(greeting("Amy"));
